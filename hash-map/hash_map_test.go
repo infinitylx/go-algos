3b533a629159e93c9d325bf518038470710bce0c
InfinitylX <infinity89@gmail.com>
2019-02-09 15:51:29 +0100

    Update tests for hash map.

--- hash-map/hash_map_test.go
+++ hash-map/hash_map_test.go
@@ -1,34 +1,53 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPutGet(t *testing.T) {
 	var ht = New()
-	var key = "key1"
-	var value = "value1"
-	ht.Put(key, value)
-
-	tvalue, err := ht.Get(key)
-	if err != nil {
-		t.Error("Get don't work.")
-		t.Error(err)
+	var keys = []string{"key1", "key2", "key3"}
+	var values = []string{"value1", "value2", "value3"}
+	for i := 0; i < len(keys); i++ {
+		ht.Put(keys[i], values[i])
 	}
-	if tvalue != value {
-		t.Error("Get incorrect value!")
-		t.Error(tvalue)
+
+	for i := 0; i < len(keys); i++ {
+		tvalue, err := ht.Get(keys[i])
+		if err != nil {
+			t.Error("Get don't work.")
+			t.Error(err)
+		}
+		if tvalue != values[i] {
+			t.Error("Get incorrect value!")
+			t.Error(tvalue)
+		}
 	}
 }
 
 func TestRemove(t *testing.T) {
 	var ht = New()
-	var key = "key1"
-	var value = "value1"
-	ht.Put(key, value)
+	var keys = []string{"key1", "key2", "key3"}
+	var values = []string{"value1", "value2", "value3"}
+	for i := 0; i < len(keys); i++ {
+		ht.Put(keys[i], values[i])
+	}
 
-	ht.Remove(key)
-	_, err := ht.Get(key)
+	ht.Remove(keys[1])
+	_, err := ht.Get(keys[1])
 
 	if err == nil {
 		t.Error("Get non nil error.")
 	}
+	err = ht.Remove(keys[1])
+
+	if err == nil {
+		t.Error("Get non nil error.")
+	}
+}
+
+func TestCollision(t *testing.T) {
+	// put multiple keys under one index
+	// check get
+	// check remove
 }
